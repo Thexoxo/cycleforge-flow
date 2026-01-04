8d9837efde20521f0f7ef665882e7d0b5f43616a
xoxo <95187401+Thexoxo@users.noreply.github.com>
2026-01-04 14:26:03 +0100

    Refactor reset.sh to install packages and dotfiles
    Removed the full reset functionality and added installation commands for various packages and dotfiles.

--- reset.sh
+++ reset.sh
@@ -1,9 +1,7 @@
-#!/bin/bash
-# Full reset debian-z : tous paquets + configs
-PAQUETS="i3-wm i3status ly alacritty tmux polybar rofi brave-browser firefox-esr vim stow feh sxhkd brave-browser neovim picom dunst"
-apt purge $PAQUETS -y
-apt autoremove --purge -y && apt autoclean
-rm -rf ~/.config/i3 ~/.config/alacritty ~/.tmux* ~/.vim* ~/.bashrc ~/.profile ~/.zshrc ~/.xinitrc ~/.Xresources ~/.fehbg ~/.config/polybar ~/.config/rofi ~/.config/sxhkd ~/.local/share/applications/*
-rm -f /etc/X11/xinit/xclients /etc/X11/xorg.conf.d/*
-systemctl set-default multi-user.target
-echo "FULL RESET debian-z OK. reboot -> TTY pur (Ctrl+Alt+F2)."
+apt install stow tmux zsh nodejs npm neovim ripgrep -y  # Ghostty Debian ? AppImage
+git clone https://codeberg.org/philhansch/dotfiles ~/dotfiles-phansch
+cd ~/dotfiles-phansch && stow i3 tmux zsh neovim
+mkdir -p ~/.local/share/nvim/lazy
+git clone https://github.com/ravitemer/mcphub.nvim ~/.local/share/nvim/lazy/mcphub.nvim
+nvim +Lazy  # Lazy.nvim auto-détecte mcphub → :Lazy sync
+:MCPHub     # Marketplace prêt !
